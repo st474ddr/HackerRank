50206766d3f5606e165f49fe6a7e028a5e0678f5
Hikke <l7365714@gmail.com>
2020-02-27 11:26:48 +0800

    update input testdata

--- DataBase/Interviews/Interviews.sql
+++ DataBase/Interviews/Interviews.sql
@@ -1,19 +1,23 @@
 set sql_mode = '';
-select contest_id,hacker_id,name,all_s,all_as,all_v
-from Contests
-join Colleges using
-select college_id,sum(tot_s) as all_s,sum(tot_as) as all_as,sum(tot_v) as all_v,sum(tot_uv) as all_uv
-from Challenges cha
-join (
-    select challenge_id,sum(total_views) as tot_v,sum(total_unique_views)as tot_uv 
-    from View_Stats 
-    group by challenge_id
-) v
-on (cha.challenge_id = v.challenge_id)
-join (
-    select challenge_id,sum(total_submissions) as tot_s,sum(total_accepted_submissions)as tot_as 
-    from Submission_Stats  
-    group by challenge_id
-) s
-on (cha.challenge_id = s.challenge_id)
-group by college_id
+select contest_id,hacker_id,name,all_s,all_as,all_v,all_uv
+from Contests con
+join Colleges col using (contest_id)
+join(
+    select college_id,sum(tot_s) as all_s,sum(tot_as) as all_as,sum(tot_v) as all_v,sum(tot_uv) as all_uv
+    from Challenges cha
+    join (
+        select challenge_id,sum(total_views) as tot_v,sum(total_unique_views)as tot_uv 
+        from View_Stats 
+        group by challenge_id
+    ) v
+    on (cha.challenge_id = v.challenge_id)
+    join (
+        select challenge_id,sum(total_submissions) as tot_s,sum(total_accepted_submissions)as tot_as 
+        from Submission_Stats  
+        group by challenge_id
+    ) s
+    on (cha.challenge_id = s.challenge_id)
+    group by college_id
+) g
+using (college_id)
+group by con.contest_id
